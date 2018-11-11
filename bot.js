fa6772371d421a9aba78adbe8f79cdf77f6ea5a0
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-11-11 13:58:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`╔ vigo stars ╗`,"http://twitch.tv/S-F")
+client.user.setGame(`╔ vigo stars ╗`)
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
